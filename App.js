22885321d17a100848264735ae6e2ca4165d60b3
Carlos Kelley <carloskelley@protonmail.com>
2022-12-04 21:55:42 -0600

    intro to native styling

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.SafeArea}>
+      <View style={[styles.container, styles.pink]}>
         <Text>Hello World</Text>
       </View>
     </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  pink: {
+    backgroundColor: 'pink',
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  SafeArea: {
+    flex: 2,
+  },
+});
 export default App;
